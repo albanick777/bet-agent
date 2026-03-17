98e0ce868b34e4f4f70de433da38d136f6467ba5
albanick777 <albanick777@yahoo.com>
2026-03-17 22:57:56 +0300

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,12 +1,20 @@
 export default async function handler(req, res) {
-  const apiKey = process.env.API_FOOTBALL_KEY;
+  try {
+    const apiKey = process.env.API_FOOTBALL_KEY;
 
-  const response = await fetch('https://v3.football.api-sports.io/fixtures?next=20', {
-    headers: {
-      'x-apisports-key': apiKey
-    }
-  });
+    const response = await fetch("https://v3.football.api-sports.io/fixtures?next=5", {
+      headers: {
+        "x-apisports-key": apiKey,
+      },
+    });
 
-  const data = await response.json();
-  res.status(200).json(data);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: "API ERROR",
+      details: error.message,
+    });
+  }
 }
