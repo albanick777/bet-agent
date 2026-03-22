387ff9cb59b66c2ce598526f7ab777f791b4a34c
albanick777 <albanick777@yahoo.com>
2026-03-22 22:37:30 +0300

    Fix fetch syntax final

--- api/telegram.js
+++ api/telegram.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 const vipData = await check.json();
 
 if (vipData.access === "BLOCKED") {
-  await fetch(`https://api.telegram.org/bot${token}/sendMessage`
+await fetch(`https://api.telegram.org/bot${token}/sendMessage`, { 
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
