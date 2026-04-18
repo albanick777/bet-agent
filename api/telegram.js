3de05e4de2545b5c5bf88bcead9f08355d7b2870
albanick777 <albanick777@yahoo.com>
2026-04-18 21:45:26 +0300

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -7,8 +7,8 @@ let lastSentDate = null;
 export default async function handler(req, res) {
   const todayDate = new Date().toISOString().slice(0, 10);
 
-const force = req.query?.force === "true";
-if (lastSentDate === todayDate && !force) {
+
+if (lastSentDate === todayDate) {
     return res.status(200).json({ status: "ALREADY_SENT", date: todayDate });
   }
 
